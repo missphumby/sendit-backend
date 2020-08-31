4e47c24a4217e14135ea52aa4060e278b178058e
Funby <jamiuaminat@yahoo.com>
2020-08-31 14:21:22 +0100

    hh

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(toastr())
 app.use((req, res, next)=>{
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Credentials: true") 
-    res.header("Access-Control-Allow-headers", 
+    res.header("Access-Control-Allow-Headers", 
     "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     // res.header("Access-Control-Max-Age", "1000")
     if (req.method == "OPTIONS"){
