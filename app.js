827eeb8a67bfac1dea38683a5a148e85d4a3f855
Funby <jamiuaminat@yahoo.com>
2020-08-31 18:20:48 +0100

    ll

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.use(cors())
 // app.use(toastr())
 //solving cors issue
 app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Credentials: true") 
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080")
+    // res.header("Access-Control-Allow-Credentials: true") 
     res.header("Access-Control-Allow-Headers", 
     "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     // res.header("Access-Control-Max-Age", "1000")
     if (req.method == "OPTIONS"){
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET, OPTIONS")
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
         return res.status(200).json({})
     }
     next()
