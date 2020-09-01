54da2e23b6e69b3b51dbe1deb1b456348993293e
Funby <jamiuaminat@yahoo.com>
2020-09-01 13:10:14 +0100

    kkk

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cors())
 //solving cors issue
 app.use((req, res, next)=>{
     res.header("Access-Control-Allow-Origin", "*")
-    // res.header("Access-Control-Allow-Credentials: true") 
+    res.header("Access-Control-Allow-Credentials: true") 
     res.header("Access-Control-Allow-Headers", 
     "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     // res.header("Access-Control-Max-Age", "1000")
