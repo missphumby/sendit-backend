8185d39b51628a279e8b7976a8af3737f1955846
Funby <jamiuaminat@yahoo.com>
2020-11-05 17:42:44 +0100

    kkk

--- routes/forgotpassword.js
+++ routes/forgotpassword.js
@@ -30,8 +30,8 @@ const nodemailer = require('nodemailer');
         const transporter = nodemailer.createTransport({
           service: 'gmail',
           auth: {
-            user: `${process.env.EMAIL_ADDRESS}`,
-            pass: `${process.env.EMAIL_PASSWORD}`,
+            user: process.env.EMAIL_ADDRESS,
+            pass: process.env.EMAIL_PASSWORD,
           },
         });
 
