918760e0836102e7630c5c42bee89ce988f36f9c
Funby <jamiuaminat@yahoo.com>
2020-11-06 13:53:31 +0100

    ppp

--- routes/forgotpassword.js
+++ routes/forgotpassword.js
@@ -42,7 +42,7 @@ const nodemailer = require('nodemailer');
           text:
             'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
             + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
-            + `https://send-it-app.herokuapp.com/reset/${token}\n\n`
+            + `https://send-itt-react.herokuapp.com/reset/${token}\n\n`
             + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
         };
 
